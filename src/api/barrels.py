8a5e1ff783d3eb2f3b5fe0fa69e628a9de58484e
Yakkaya <yakkaya@calpoly.edu>
2024-10-02 10:35:30 -0700

    add inventory management to be handled by barrels/plan api

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -25,15 +25,18 @@ class Barrel(BaseModel):
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     update_expression = sqlalchemy.text(f"""
         UPDATE {INVENTORY_TABLE_NAME} 
-        SET num_green_ml = num_green_ml + :ml
+        SET num_green_ml = num_green_ml + :ml, 
+        gold = gold - :barrel_cost
     """)
 
     for barrel in barrels_delivered:
         if barrel.sku == "SMALL_GREEN_BARREL":
             ml = barrel.ml_per_barrel
+            barrel_cost = barrel.quantity * barrel.price
             with db.engine.begin() as connection:
                 connection.execute(update_expression, {
-                "ml": ml
+                "ml": ml,
+                "barrel_cost": barrel_cost
             })
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
@@ -46,10 +49,6 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     print(wholesale_catalog)
     request_quantity = 0
     select_expression = f"SELECT num_green_potions, gold FROM {INVENTORY_TABLE_NAME}"
-    update_gold_expression = sqlalchemy.text(f"""
-        UPDATE {INVENTORY_TABLE_NAME} 
-        SET gold = gold - :barrel_cost
-    """)
 
     for barrel in wholesale_catalog:
         # Version 1 only supports green potions
@@ -59,15 +58,10 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
                 row = result.fetchone()
                 num_green_potions_inventory = row[0]
                 gold_inventory = row[1]
-                barrel_cost = 0
                 if num_green_potions_inventory < 10 and gold_inventory >= barrel.price:
                     # if the number of green potions is less than 10 and gold in inventory is 
                     # sufficient, request a new barrel
                     request_quantity = 1
-                    barrel_cost = request_quantity * barrel.price
-                    result = connection.execute(update_gold_expression, {
-                        "barrel_cost": barrel_cost
-                    }) 
                 print(f"Requested {request_quantity} small green barrel(s). Deducted gold by {barrel_cost}")
 
     return [
