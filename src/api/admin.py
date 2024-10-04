ad49e146307ae4e4d661c5ddf77d7431204599d0
Yakkaya <yakkaya@calpoly.edu>
2024-10-04 10:48:12 -0700

    updated barrel logic

--- src/api/admin.py
+++ src/api/admin.py
@@ -18,7 +18,16 @@ def reset():
     Reset the game state. Gold goes to 100, all potions are removed from
     inventory, and all barrels are removed from inventory. Carts are all reset.
     """
-    update_expression = f"UPDATE {INVENTORY_TABLE_NAME} SET num_green_potions = 0, num_green_ml = 0, gold = 100"
+    update_expression = f"""UPDATE {INVENTORY_TABLE_NAME} SET num_green_potions = 0, 
+    num_green_ml = 0,
+    num_blue_potions = 0, 
+    num_blue_ml = 0, 
+    num_red_potions = 0, 
+    num_red_ml = 0,  
+    num_dark_potions = 0,
+    num_dark_ml = 0,
+    gold = 100"""
+    
     with db.engine.begin() as connection:
         result = connection.execute(sqlalchemy.text(update_expression))
     return "OK"
--- src/api/barrels.py
+++ src/api/barrels.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 from src.api import auth
 
 INVENTORY_TABLE_NAME = "global_inventory"
+INVENTORY_ML_TYPES = ["num_green_ml", "num_red_ml", "num_blue_ml", "num_dark_ml"]
+INVENTORY_POTION_TYPES = ["num_green_potions", "num_red_potions", "num_blue_potions", "num_dark_potions"]
 
 router = APIRouter(
     prefix="/barrels",
@@ -21,53 +23,84 @@ class Barrel(BaseModel):
 
     quantity: int
 
+
+class PurchaseRequest(BaseModel):
+    sku: str
+    quantity: int
+
+
 @router.post("/deliver/{order_id}")
 def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
-    update_expression = sqlalchemy.text(f"""
-        UPDATE {INVENTORY_TABLE_NAME} 
-        SET num_green_ml = num_green_ml + :ml, 
-        gold = gold - :barrel_cost
-    """)
-
     for barrel in barrels_delivered:
-        if barrel.sku == "SMALL_GREEN_BARREL":
-            ml = barrel.ml_per_barrel
-            barrel_cost = barrel.quantity * barrel.price
-            with db.engine.begin() as connection:
-                connection.execute(update_expression, {
-                "ml": ml,
-                "barrel_cost": barrel_cost
-            })
+        ml_type_to_update = get_ml_attribute_from_sku(barrel.sku)
+        ml = barrel.ml_per_barrel
+        barrel_cost = barrel.quantity * barrel.price
+
+        update_expression = sqlalchemy.text(f"""
+        UPDATE {INVENTORY_TABLE_NAME} 
+        SET {ml_type_to_update} = {ml_type_to_update} + :ml, 
+        gold = gold - :barrel_cost""")
+
+        with db.engine.begin() as connection:
+            connection.execute(update_expression, {
+            "ml": ml,
+            "barrel_cost": barrel_cost
+        })
+
     print(f"barrels delievered: {barrels_delivered} order_id: {order_id}")
 
     return "OK"
 
+
+
 # Gets called once a day
 @router.post("/plan")
-def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
-    """ """
+def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]) -> list[PurchaseRequest]:
+    requests = []
     print(wholesale_catalog)
-    request_quantity = 0
-    select_expression = f"SELECT num_green_potions, gold FROM {INVENTORY_TABLE_NAME}"
 
     for barrel in wholesale_catalog:
-        # Version 1 only supports green potions
-        if barrel.sku == "SMALL_GREEN_BARREL": 
-            with db.engine.begin() as connection:
-                result = connection.execute(sqlalchemy.text(select_expression))
-                row = result.fetchone()
-                num_green_potions_inventory = row[0]
-                gold_inventory = row[1]
-                if num_green_potions_inventory < 10 and gold_inventory >= barrel.price:
-                    # if the number of green potions is less than 10 and gold in inventory is 
-                    # sufficient, request a new barrel
-                    request_quantity = 1
-                print(f"Requested {request_quantity} small green barrel(s). Deducted gold by {barrel_cost}")
+        potion_type_to_check = get_potion_type_from_sku(barrel.sku) 
+        select_expression = f"SELECT {potion_type_to_check}, gold FROM {INVENTORY_TABLE_NAME}"
+        with db.engine.begin() as connection:
+            potion_type_to_check = get_potion_type(barrel.potion_type)
+            result = connection.execute(sqlalchemy.text(select_expression))
+            row = result.fetchone()
+            potion_inventory = row[0]
+            gold_inventory = row[1]
+            if potion_inventory < 10 and gold_inventory >= barrel.price:
+                # if the number of green potions is less than 10 and gold in inventory is 
+                # sufficient, request a new barrel
+                purchase_request = PurchaseRequest(
+                sku=barrel.sku,
+                quantity=request_quantity
+                )
+                requests.append(purchase_request)
+    print(requests)
+    return requests
 
-    return [
-        {
-            "sku": "SMALL_GREEN_BARREL",
-            "quantity": request_quantity,
-        }
-    ]
 
+def get_ml_attribute_from_sku(barrel_sku: str) -> str:
+    if "red" in barrel_sku.lower():
+        return INVENTORY_ML_TYPES[1]
+    elif "green" in barrel_sku.lower():
+        return INVENTORY_ML_TYPES[0]
+    elif "blue" in barrel_sku.lower():
+        return INVENTORY_ML_TYPES[2]
+    elif "dark" in barrel_sku.lower():
+        return INVENTORY_ML_TYPES[3]
+    else:
+        raise ValueError(f"Invalid SKU: {barrel_sku} does not contain a valid color")
+
+
+def get_potion_type(potion_type: list[int]) -> str:
+    if potion_type == [1, 0, 0, 0]:
+        return INVENTORY_POTION_TYPES[1]
+    elif potion_type == [0, 1, 0, 0]:
+        return INVENTORY_POTION_TYPES[0]
+    elif potion_type == [0, 0, 1, 0]:
+        return INVENTORY_POTION_TYPES[2]
+    elif potion_type == [0, 0, 0, 1]:
+        return INVENTORY_POTION_TYPES[3]
+    else:
+        raise ValueError(f"Invalid potion type: {potion_type} is not a valid potion type")
